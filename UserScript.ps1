99f49eb6582f117cb1b21b96a72a1fa79271dca2
Jens Vercruysse <jensvercruysse@outlook.com>
2019-10-21 13:52:47 +0200

    major refactor + bugs fixed

--- UserScript.ps1
+++ UserScript.ps1
@@ -1,21 +1,75 @@
+Import-Module activedirectory
+
 $CSVPath = "C:/Scripts/personeel.csv"
 
-Import-Module activedirectory
-function DisableInvalidUsers {
-    $ValidUsers = Import-Csv -Delimiter ";" -Path $CSVPath | Select-Object -ExpandProperty Account
-    $InvalidUsers = Get-ADGroupMember "Domain Users" | Where-Object distinguishedName -like *OU=JJEAfdeling* | Where-Object { $ValidUsers -notcontains $_.SamAccountName }
-    foreach ($InvalidUser in $InvalidUsers) {
-        Disable-ADAccount -identity $InvalidUser -Confirm:$false
-        $DisabledUser = $InvalidUser.SamAccountName
-        Write-Output "Disabled $DisabledUser in AD"
-    }      
+Function Disable-User {
+    Disable-ADAccount -identity $InvalidUser -Confirm:$false
+    $DisabledUser = $InvalidUser.SamAccountName
+    Write-Output "$DisabledUser has been disabled!"
+}      
+
+Function Update-Groups {
+    param([parameter(Mandatory)][String]$Account, [String[]]$GroupsArray)
+
+    Get-ADUser -Identity $Account -Properties MemberOf | ForEach-Object {
+        $_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Confirm:$false
+    }
+    foreach ($Group in $GroupsArray) {
+        Add-ADGroupMember -Identity $Group -Members $Account
+    }
 }
 
-DisableInvalidUsers
+Function Update-User {
+    param([parameter(Mandatory)][String]$Account, [String]$OU, [String[]]$GroupsArray)
+
+    $UserDN = (Get-ADUser -Identity $Account).distinguishedName
+
+    Move-ADObject -Identity $UserDN -TargetPath "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
+    Update-Groups -Account $Account -GroupsArray $GroupsArray
+    Enable-ADAccount -Identity $Account 
+    Set-ADOrganizationalUnit -Identity "OU=$ManagerOf, OU=JJEAfdeling, DC=CynMedJJE, DC=be" -ManagedBy $Account
+
+    Write-Output "$Account has been updated."
+}
+
+Function Initialize-User {
+    param([parameter(Mandatory)][String]$Voornaam, [string]$Naam, [String]$Account, [String]$OU, [String[]]$GroupsArray)
+
+    $FullName = "$Voornaam $Naam"
+    $Email = $Voornaam + "." + $Naam + "@CynMedJJE.be"
+    $Password = $Voornaam + "@CynMedJJE"
+
+    New-ADUser `
+        -Name $FullName `
+        -GivenName $Voornaam `
+        -Surname $Naam `
+        -SamAccountName $Account `
+        -EmailAddress $Email `
+        -AccountPassword (ConvertTo-SecureString -String $Password -AsPlainText -Force) `
+        -ChangePasswordAtLogon: 1 `
+        -Path "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
+
+    Update-Groups -Account $Account -GroupsArray $GroupsArray
+
+    Enable-ADAccount -Identity $Account    
+    Write-Output "[$Voornaam $Naam] has been created and added to the $OU OU."
+}
 
 $Personeel = Import-csv -Delimiter ";" -Path $CSVPath
+$NumberOfAdded = 0
+$NumberOfUpdated = 0
+$NumberOfEnabled = 0
+$NumberOfDisabled = 0
+
+$ValidUsers = Import-Csv -Delimiter ";" -Path $CSVPath | Select-Object -ExpandProperty Account
+$InvalidUsers = Get-ADGroupMember "Domain Users" | Where-Object distinguishedName -like *OU=JJEAfdeling* | Where-Object { $ValidUsers -notcontains $_.SamAccountName }
+foreach ($InvalidUser in $InvalidUsers) {
+    Disable-User
+    $NumberOfDisabled++
+}
 
 foreach ($User in $Personeel) {
+
     $Naam = $User.Naam
     $Voornaam = $User.Voornaam
     $Account = $User.Account
@@ -24,173 +78,83 @@ foreach ($User in $Personeel) {
     $Boekhouding = $User.Boekhouding
     $Logistiek = $User.Logistiek
     $ImportExport = $User.ImportExport
-    $Email = $Voornaam + "." + $Naam + "@CynMedJJE.be"
-    $FullName = "$Voornaam $Naam"
 
-    if ($Voornaam.length -lt 3 -or $Naam.length -lt 3){
-    $TempVoornaam = $Voornaam
-    $TempNaam = $Naam
-    Do{
-    $TempVoornaam = $TempVoornaam + "+"
-    $TempVoornaam = $TempVoornaam + "+"
+    if ($Manager -eq "X") {
+        if ($IT -eq "X") {
+            $OU = "Managers"
+            $ManagerOf = "IT"
+            $GroupsArray = "Managers,IT" -split ","
+        }
+        elseif ($Boekhouding -eq "X") {
+            $OU = "Managers"
+            $ManagerOf = "Boekhouding"
+            $GroupsArray = "Managers,Boekhouding" -split ","
+        }
+        elseif ($Logistiek -eq "X") {
+            $OU = "Managers"
+            $ManagerOf = "Logistiek"
+            $GroupsArray = "Managers,Logistiek" -split ","
+        }
+        elseif ($ImportExport -eq "X") {
+            $OU = "Managers"
+            $ManagerOf = "ImportExport"
+            $GroupsArray = "Managers,ImportExport" -split ","
+        }
+        else {
+            $OU = "Managers"
+            $ManagerOf = "Managers"
+            $GroupsArray = "Managers" -split ","
+        }
     }
-    Until($TempVoornaam -ge 3 -and $TempNaam -ge 3)
-    $Password = $TempVoornaam.substring(0, 3) + "&" + $TempNaam.substring(0, 3)
-    Write-Output "[$Voornaam $Naam] has a name shorter than 3 characters, standard password has been adjusted!"
-    } else {
-    $Password = $Voornaam.substring(0, 3) + "&" + $Naam.substring(0, 3)
+    else {
+        if ($IT -eq "X") {
+            $OU = "IT"
+            $GroupsArray = "IT" -split ","
+        }
+        elseif ($Boekhouding -eq "X") {
+            $OU = "Boekhouding"
+            $GroupsArray = "Boekhouding" -split ","
+        }
+        elseif ($Logistiek -eq "X") {
+            $OU = "Logistiek"
+            $GroupsArray = "Logistiek" -split ","
+        }
+        elseif ($ImportExport -eq "X") {
+            $OU = "ImportExport"
+            $GroupsArray = "ImportExport" -split ","
+        }
     }
 
     if (Get-ADUser -F { SamAccountName -eq $Account }) {
-        $UserDN = (Get-ADUser -Identity $Account).distinguishedName
-        $OU = ""
-        $GroupsArray = "" -split ","
-        if ($Manager -eq "X") {
-            if ($IT -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "IT"
-                $GroupsArray = "Managers,IT" -split ","
-            }
-            elseif ($Boekhouding -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "Boekhouding"
-                $GroupsArray = "Managers,Boekhouding" -split ","
-            }
-            elseif ($Logistiek -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "Logistiek"
-                $GroupsArray = "Managers,Logistiek" -split ","
-            }
-            elseif ($ImportExport -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "ImportExport"
-                $GroupsArray = "Managers,ImportExport" -split ","
-            }
-            else {
-                $OU = "Managers"
-                $ManagerOf = "Managers"
-                $GroupsArray = "Managers" -split ","
-            }
-            Get-ADUser -Identity $Account -Properties MemberOf | ForEach-Object {
-                $_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Confirm:$false
-            }
-            foreach ($Group in $GroupsArray) {
-                Add-ADGroupMember -Identity $Group -Members $Account
-            }
-            Move-ADObject -Identity $UserDN  -TargetPath "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
-            Write-Output "[$Voornaam $Naam] has been updated"
+
+        $User = Get-ADUser -Identity $Account -Properties DistinguishedName 
+        $UserOU = ($User.DistinguishedName -split "=", 3)[-1]
+        $OldOU = ($UserOU -split ",", 2)[0]
+        
+        if ($(Get-ADUser -Identity $Account).enabled -eq $false) {
+            Enable-ADAccount -Identity $Account
+            Write-Output "$Account has been enabled!"
+            $NumberOfEnabled++
+        }
+        
+        if ($OldOU -eq $OU) {
+            Write-Output "$Account is up to date."
         }
         else {
-            if ($IT -eq "X") {
-                $OU = "IT"
-                $GroupsArray = "IT" -split ","
-            }
-            elseif ($Boekhouding -eq "X") {
-                $OU = "Boekhouding"
-                $GroupsArray = "Boekhouding" -split ","
-            }
-            elseif ($Logistiek -eq "X") {
-                $OU = "Logistiek"
-                $GroupsArray = "Logistiek" -split ","
-            }
-            elseif ($ImportExport -eq "X") {
-                $OU = "ImportExport"
-                $GroupsArray = "ImportExport" -split ","
-            }
-            Get-ADUser -Identity $Account -Properties MemberOf | ForEach-Object {
-                $_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Confirm:$false
-            }
-            foreach ($Group in $GroupsArray) {
-                Add-ADGroupMember -Identity $Group -Members $Account
-            }
-            Move-ADObject -Identity $UserDN -TargetPath "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
-            Write-Output "[$Voornaam $Naam] has been updated"
+            Update-User -Account $Account -OU $OU -GroupsArray $GroupsArray
+            $NumberOfUpdated++
         }
-    }          
+    }
     else {
-        $OU = ""
-        $GroupsArray = "" -split ","
-        if ($Manager -eq "X") {
-            if ($IT -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "IT"
-                $GroupsArray = "Managers,IT" -split ","
-            }
-            elseif ($Boekhouding -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "Boekhouding"
-                $GroupsArray = "Managers,Boekhouding" -split ","
-            }
-            elseif ($Logistiek -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "Logistiek"
-                $GroupsArray = "Managers,Logistiek" -split ","
-            }
-            elseif ($ImportExport -eq "X") {
-                $OU = "Managers"
-                $ManagerOf = "ImportExport"
-                $GroupsArray = "Managers,ImportExport" -split ","
-            }
-            else {
-                $OU = "Managers"
-                $ManagerOf = "Managers"
-                $GroupsArray = "Managers" -split ","
-            }
-            New-ADUser `
-                -Name $FullName `
-                -GivenName $Voornaam `
-                -Surname $Naam `
-                -SamAccountName $Account `
-                -EmailAddress $Email `
-                -AccountPassword (ConvertTo-SecureString -String $Password -AsPlainText -Force) `
-                -ChangePasswordAtLogon: 1 `
-                -Path "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
-
-            Enable-ADAccount -Identity $Account    
-            Get-ADUser -Identity $Account -Properties MemberOf | ForEach-Object {
-                $_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Confirm:$false
-            }  
-            foreach ($Group in $GroupsArray) {
-                Add-ADGroupMember -Identity $Group -Members $Account
-            }
-            Set-ADOrganizationalUnit -Identity "OU=$ManagerOf, OU=JJEAfdeling, DC=CynMedJJE, DC=be" -ManagedBy $Account
-            Write-Output "[$Voornaam $Naam] has been created and added to the $OU OU"
-        }
-        else {
-            if ($IT -eq "X") {
-                $OU = "IT"
-                $GroupsArray = "IT" -split ","
-            }
-            elseif ($Boekhouding -eq "X") {
-                $OU = "Boekhouding"
-                $GroupsArray = "Boekhouding" -split ","
-            }
-            elseif ($Logistiek -eq "X") {
-                $OU = "Logistiek"
-                $GroupsArray = "Logistiek" -split ","
-            }
-            elseif ($ImportExport -eq "X") {
-                $OU = "ImportExport"
-                $GroupsArray = "ImportExport" -split ","
-            }
-            New-ADUser `
-                -Name $FullName `
-                -GivenName $Voornaam `
-                -Surname $Naam `
-                -SamAccountName $Account `
-                -EmailAddress $Email `
-                -AccountPassword (ConvertTo-SecureString -String $Password -AsPlainText -Force) `
-                -ChangePasswordAtLogon: 1 `
-                -Path "OU=$OU, OU=JJEAfdeling, DC=CynMedJJE, DC=be"
-
-            Enable-ADAccount -Identity $Account    
-            Get-ADUser -Identity $Account -Properties MemberOf | ForEach-Object {
-                $_.MemberOf | Remove-ADGroupMember -Members $_.DistinguishedName -Confirm:$false
-            } 
-            foreach ($Group in $GroupsArray) {
-                Add-ADGroupMember -Identity $Group -Members $Account
-            }
-            Write-Output "[$Voornaam $Naam] has been created and added to the $OU OU"
-        }
-    }          
+        Initialize-User -Voornaam $Voornaam -Naam $Naam -Account $Account -OU $OU -GroupsArray $GroupsArray
+        $NumberOfAdded++
+    }
+    Set-ADOrganizationalUnit -Identity "OU=$ManagerOf, OU=JJEAfdeling, DC=CynMedJJE, DC=be" -ManagedBy $Account
+    Set-ADGroup -Identity $ManagerOf -ManagedBy $Account
 }
+
+Write-Output ""
+Write-Output "$NumberOfAdded Account(s) added."
+Write-Output "$NumberOfUpdated Account(s) updated."
+Write-Output "$NumberOfEnabled Account(s) enabled."
+Write-Output "$NumberOfDisabled Account(s) disabled."
