7909a50a3a96a9878935b35c80d4738b472a829d
Jens Vercruysse <36663522+JensVercruysse@users.noreply.github.com>
2019-10-21 13:58:35 +0200

    Update DeleteAllUsers.ps1

--- DeleteAllUsers.ps1
+++ DeleteAllUsers.ps1
@@ -6,12 +6,6 @@ foreach ($User in $Personeel) {
     $Naam = $User.Naam
     $Voornaam = $User.Voornaam
     $Account = $User.Account
-    $Manager = $User.Manager
-    $IT = $User.IT
-    $Boekhouding = $User.Boekhouding
-    $Logistiek = $User.Logistiek
-    $ImportExport = $User.ImportExport
-    $FullName = $Voornaam + $Naam
 
     Remove-ADUser -Identity $Account -Confirm:$false
     Write-Output "[$Voornaam $Naam] has been removed"
